f797687f10366e0498f35e0b56f8ffcae3eca37f
Anirudh Sivaraman <sk.anirudh@gmail.com>
2019-05-15 16:45:19 -0400

    Move pipeline_stage into its own class.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
+mod pipeline_stage;
+
 fn main() {
   // List of all stages
-  let pipeline_stages = Vec::<PipelineStage>();
+  let pipeline_stages = vec![pipeline_stage::PipelineStage{};10];
   // Outer simulation loop
-  for tick in 0..10000 {
+  for tick in 0..100 {
     println!("Tick number {}", tick);
-    for stage in stages {
+    for stage in &pipeline_stages {
       stage.tick(tick);
     }
   }
--- src/pipeline_stage.rs
+++ src/pipeline_stage.rs
@@ -1,1 +1,9 @@
 // TODO: File representing a single pipeline stage
+#[derive(Clone)]
+pub struct PipelineStage {
+
+}
+
+impl PipelineStage {
+  pub fn tick(&self, tick : u8) { println!("Current tick is {}", tick)}
+}
