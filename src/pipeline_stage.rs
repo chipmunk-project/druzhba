83f00246d086dc39c5d30f05d9d41ff96ae27ce7
Aatish Varma <aatish17varma@gmail.com>
2019-06-24 14:05:06 -0400

    full pipeline_stage
     Author:    Aatish Varma <aatish17varma@gmail.com>

--- src/pipeline_stage.rs
+++ src/pipeline_stage.rs
@@ -1,22 +1,41 @@
-// Represents a stage in the pipeline
+// TODO: File representing a single pipeline stage
 
-use crate::phv::Phv;
+mod alu;
+mod phv; 
 
-#[derive(Clone)]
+use rand::{thread_rng, Rng};
 
-pub struct PipelineStage {
-  // TODO: Add vector of ALUs
+pub struct Stage{
+    atoms: Vec<alu::ALU>
 }
 
-impl PipelineStage {
+impl Stage{
 
-  // TODO: Create 1 parameter constructor
-  // that takes in vector of ALUs
+    /*Ask Anirudh how neccesary it is to have an initializer list constructor.*/
 
-  pub fn tick(&self, packet : Phv) -> Phv { 
-    // TODO: Complete. Should iterate through
-    // ALUs in random order and each take in the
-    // Phv
-    packet
-  }
+    pub fn new(vec_of_atoms : Vec<alu::ALU>) -> Self {
+        return Stage{ atoms: vec_of_atoms };
+    }
+
+    pub fn tick(single_packet : phv::Phv, &self) -> phv::Phv {
+        if (single_packet.bubble){
+            return phv::Phv::new(); 
+        }
+        else{
+            let ret = phv::Phv::new();
+            thread_rng().shuffle(self.atoms);
+            for atom in self.atoms {
+                let current_phv = single_packet;
+                atom(current_phv);
+                ret += current_phv.clone();
+            }
+            return ret;
+        }
+    }
 }
+
+fn main(){
+    println!("hello world");
+}
+
+
