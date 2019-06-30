6d399bf26c5c3aee51954386fca66c556c12272a
Mike Wong <mdw362@nyu.edu>
2019-06-30 08:57:38 -0400

    Slight changes and added few more comments

--- src/alu.rs
+++ src/alu.rs
@@ -1,1 +1,53 @@
 // TODO: File representing stateful and stateless ALUs.
+
+use crate::phv_container::PhvContainer;
+use crate::phv::Phv;
+
+
+#[derive(Clone)]
+pub struct ALU{
+
+    // Function that ALU will use against incoming Phvs 
+    sequential_function :
+        fn (&mut Phv, 
+            &mut PhvContainer <i32>, 
+            &mut PhvContainer <Vec <i32> >),
+
+    // state_scalar and state_array capture the internal
+    // state of the atom that map String fields to i32
+    // and Vec <i32> respectively
+    state_scalar : PhvContainer <i32>,
+    state_array : PhvContainer <Vec <i32> >,
+        
+}
+
+impl ALU {
+       
+    pub fn new (function : fn (&mut Phv, 
+                               &mut PhvContainer <i32>, 
+                               &mut PhvContainer <Vec <i32> >),
+                new_state_scalar : PhvContainer <i32>,
+                new_state_array : PhvContainer <Vec <i32> >) 
+                -> ALU {
+
+      ALU { sequential_function : function,
+            state_scalar : new_state_scalar, 
+            state_array : new_state_array }
+    }
+
+    // Receives mutable reference to Phv and calls underlying 
+    // function, sequential_function using state_scalar and 
+    // state_array. Mutates Phv in place with appropriate 
+    // packet values.
+    pub fn run (&mut self, packet : &mut Phv) {
+      
+      assert! (!packet.is_bubble() );
+      (self.sequential_function) 
+          (packet, 
+           &mut self.state_scalar, 
+           &mut self.state_array );
+    }
+   
+}
+
+
--- src/phv.rs
+++ src/phv.rs
@@ -1,1 +1,93 @@
 // TODO: File representing a PHV
+
+use crate::phv_container::PhvContainer;
+use std::collections::HashMap;
+use std::ops::{Index, IndexMut, AddAssign};
+use std::fmt;
+
+
+pub type FieldName = String;
+pub type FieldType = i32;
+pub type PacketFieldSet = Vec <String>;
+
+
+#[derive(Clone)]
+pub struct Phv {
+  bubble : bool, // false if initialized, true otherwise
+  packet : PhvContainer <i32> // Contains packet fields/values
+}
+
+impl Phv{
+  // Constructor that initializes new Phv with a new PhvContainer.
+  // Bool bubble is set to true since values have not been
+  // initialized
+  pub fn new () -> Self {
+    let m : HashMap <FieldName, FieldType> = HashMap::new();
+    let c : PhvContainer <i32> = PhvContainer{ map : m };
+
+    Phv { bubble : true, packet : c }
+  } 
+
+  // Alternate constructor that takes in a PhvContainer
+  pub fn with_container (c : PhvContainer <i32>) -> Self{
+    Phv { bubble : false, packet : c}
+  }
+  pub fn is_bubble (&self) -> bool {
+    self.bubble
+  }
+}
+// Overloads the index operator: [ ]. Enables packet 
+// fields to be attained by using phv [fn], where fn
+// is the field name.
+impl Index<&str> for Phv{
+
+  type Output=FieldType;
+  fn index (&self, idx : &str) -> &FieldType{
+    &self.packet[idx]
+  }
+}
+// Overloads the index operator: [ ]. Enables packet
+// field values to be mutated by returning a mutable
+// reference. phv [fn] = value where fn is the fieldname
+// and value is the new value
+impl IndexMut<&str> for Phv {
+
+  fn index_mut (&mut self, idx : &str) -> &mut FieldType {
+    &mut self.packet[idx]
+  }
+}
+// Overloads the += operator. If the given packet has been
+// initialized (i.e. bubble is false) then it relies upon
+// PhvContainer's += operator.
+//
+// Note that add_assign takes ownership of the given packet
+// so ensure to clone it when using += if the ownership
+// is intended to be retained.
+impl AddAssign for Phv {
+
+  fn add_assign (&mut self, t_packet : Self){
+
+    assert! (!t_packet.is_bubble());
+    if self.bubble {
+      self.bubble = false;   
+    }
+    self.packet += t_packet.packet.clone(); 
+  }
+}
+
+// Uses the fmt functon as part of the Display trait. Allows
+// Phv to be printed with println!("{}", p) where p is a 
+// Phv. Using Display trait also enables the to_string 
+// method to be implemented automatically
+impl fmt::Display for Phv {
+  fn fmt (&self, f: &mut fmt::Formatter) -> fmt::Result {
+    let mut s : String = String::from("");
+    if self.bubble {
+      s.push_str("Bubble: true\n") ;
+    }
+
+    s.push_str (&self.packet.to_string());
+    write!(f, "{}", s)
+  }
+}
+
