c0aa8cafde466f5040a282b3155f6c7bdc3605ca
Aatish Varma <aatish17varma@gmail.com>
2019-07-07 18:54:25 -0400

    Changes to ALU File: - Included Input and Output Mux as struct fields - Changed State Scalar and State Array to a HashMap of state vars

--- src/alu.rs
+++ src/alu.rs
@@ -1,52 +1,69 @@
 
 use crate::phv_container::PhvContainer;
 use crate::phv::Phv;
+use crate::output_mux::OutputMux;
+use crate::input_mux::InputMux;
+use std::collections::HashMap;
+
+//Use a PHV Container as an object containing all states - *just for now*
+//return the previous states, unmodified or not
 
 
 #[derive(Clone)]
 pub struct ALU{
-
-    // Function that ALU will use against incoming Phvs 
-    sequential_function :
-        fn (&mut Phv, 
-            &mut PhvContainer <i32>, 
-            &mut PhvContainer <Vec <i32> >),
-
-    // state_scalar and state_array capture the internal
-    // state of the atom that map String fields to i32
-    // and Vec <i32> respectively
-    state_scalar : PhvContainer <i32>,
-    state_array : PhvContainer <Vec <i32> >,
+    
+    /*Packet containers from multiple muxs' are passed to an ALU, so should be a struct field and organized
+    as a vector of containers compared to a PHV*/
+    
+    pub sequential_function :
+        fn (&mut HashMap<String, i32>,
+            &mut Vec<PhvContainer<i32>>
+            ),
         
+    //Change this to a state-variable
+    pub state_variables : HashMap<String, i32>,
+    pub input_mux : InputMux,
+    pub output_mux : OutputMux
 }
 
 impl ALU {
        
-    pub fn new (function : fn (&mut Phv, 
-                               &mut PhvContainer <i32>, 
-                               &mut PhvContainer <Vec <i32> >),
-                new_state_scalar : PhvContainer <i32>,
-                new_state_array : PhvContainer <Vec <i32> >) 
-                -> ALU {
+    pub fn new (function : fn ( &mut HashMap<String, i32>, 
+                                &mut Vec<PhvContainer<i32>>),
+            state_vars : HashMap<String, i32>,
+            imux: InputMux,
+            omux : OutputMux) 
+            -> ALU {
 
       ALU { sequential_function : function,
-            state_scalar : new_state_scalar, 
-            state_array : new_state_array }
+            state_variables: state_vars,
+            input_mux: imux,
+            output_mux : omux }
     }
 
     // Receives mutable reference to Phv and calls underlying 
     // function, sequential_function using state_scalar and 
     // state_array. Mutates Phv in place with appropriate 
-    // packet values.
-    pub fn run (&mut self, packet : &mut Phv) {
-      
-      assert! (!packet.is_bubble() );
-      (self.sequential_function) 
-          (packet, 
-           &mut self.state_scalar, 
-           &mut self.state_array );
-    }
-}
+    // packet values. Once function is run, phv value should
+    // be passed to the output mux. 
 
+    pub fn run (&mut self, packet_fields: &mut Vec<PhvContainer<i32>>) {
+
+      (self.sequential_function) 
+          (&mut self.state_variables,
+           packet_fields
+           );
+    }
+
+    pub fn input_mux_output(&self) -> PhvContainer<i32>{
+        self.input_mux.output()
+    }
+    pub fn send_packets_to_input_mux(&mut self, values: Phv<i32>) {
+        self.input_mux.input_phv = values;   
+    }
+    pub fn send_packets_to_output_mux(&mut self, mut values: Vec<PhvContainer<i32>>) {
+        self.output_mux.input_phv_containers.append(&mut values);   
+    }  
+}
 
 
