92f34aae6408cde2f76e38cd135ccd434462f2fe
Aatish Varma <aatish17varma@gmail.com>
2019-07-07 23:53:44 -0400

    Added Stateful ALU example to prog file

--- src/program_to_run_akv.rs
+++ src/program_to_run_akv.rs
@@ -7,11 +7,22 @@ use druzhba::input_mux::InputMux;
 use druzhba::output_mux::OutputMux;
 use std::collections::HashMap;
 
-pub fn alu_fn(  state_vars: &mut HashMap<String, i32>,
+
+//Stateless ALU
+pub fn alu_stateless_fn(  state_vars: &mut HashMap<String, i32>,
                 packet : &mut Vec<PhvContainer<i32>>  ){
 
-        packet[0].field_value = packet[0].field_value * 2; 
+        packet[0].field_value = packet[0].field_value * 2;
+       
 }
+
+pub fn alu_stateful_fn(  state_vars: &mut HashMap<String, i32>,
+                packet : &mut Vec<PhvContainer<i32>>  ){
+
+        packet[0].field_value = packet[0].field_value * 3;
+        *state_vars.get_mut("count").unwrap() += 1;
+}
+
 
 
 pub fn init_pipeline() -> Pipeline {
@@ -46,8 +57,8 @@ pub fn init_pipeline() -> Pipeline {
 
     //generate ALUs for first pipeline stage
    
-    let alu_one_one : ALU = ALU{sequential_function: alu_fn, state_variables: state_vars.clone(), input_mux: alu_one_one_input_mux, output_mux: alu_one_one_output_mux};
-    let alu_one_two : ALU = ALU{sequential_function: alu_fn, state_variables: state_vars.clone(), input_mux: alu_one_two_input_mux, output_mux: alu_one_two_output_mux};
+    let alu_one_one : ALU = ALU{sequential_function: alu_stateless_fn, state_variables: state_vars.clone(), input_mux: alu_one_one_input_mux, output_mux: alu_one_one_output_mux};
+    let alu_one_two : ALU = ALU{sequential_function: alu_stateless_fn, state_variables: state_vars.clone(), input_mux: alu_one_two_input_mux, output_mux: alu_one_two_output_mux};
     
     let pipeline_stage_one : PipelineStage = PipelineStage{ atoms: vec![alu_one_one, alu_one_two] };
 
@@ -63,13 +74,13 @@ pub fn init_pipeline() -> Pipeline {
 
     //generate ALUs for second pipeline stage
     
-    let alu_two_one : ALU = ALU{sequential_function: alu_fn, state_variables: state_vars.clone(), input_mux: alu_two_one_input_mux, output_mux: alu_two_one_output_mux};
-    let alu_two_two : ALU = ALU{sequential_function: alu_fn, state_variables: state_vars.clone(), input_mux: alu_two_two_input_mux, output_mux: alu_two_two_output_mux};
+    let alu_two_one : ALU = ALU{sequential_function: alu_stateless_fn, state_variables: state_vars.clone(), input_mux: alu_two_one_input_mux, output_mux: alu_two_one_output_mux};
+    let alu_two_two : ALU = ALU{sequential_function: alu_stateless_fn, state_variables: state_vars.clone(), input_mux: alu_two_two_input_mux, output_mux: alu_two_two_output_mux};
 
     let pipeline_stage_two : PipelineStage = PipelineStage{atoms: vec![alu_two_one, alu_two_two]};
 
     //generate pipeline
-    
+
     let pipeline : Pipeline = Pipeline::with_pipeline_stages(vec![pipeline_stage_one, pipeline_stage_two]);
 
     pipeline
