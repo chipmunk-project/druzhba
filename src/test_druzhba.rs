faebe815320644610874bcf7bc6d270ba3a1b706
Mike Wong <mdw362@nyu.edu>
2019-08-22 07:30:50 -0700

    Added new test to test_druzhba

--- src/test_druzhba.rs
+++ src/test_druzhba.rs
@@ -7,6 +7,8 @@ use pipeline_stage::PipelineStage;
 use alu::ALU;
 use alu::StateVar;
 
+use std::collections::HashMap;
+
 #[test]
 fn test_pipeline() {
     // state_vars is unused
@@ -145,6 +147,149 @@ fn test_pipeline() {
         assert! (p[0].get_value() == 
                  field_values [index]*9);
         index+=1;
+    }
+}
+
+#[test]
+fn test_pipeline_hole_inputs() {
+    // state_vars is unused
+    fn alu_stateless_fn( _state_vars: &mut Vec<StateVar>,
+                         packet : &Vec<PhvContainer<i32>>) -> Vec <i32>{
+        vec! [packet[0].field_value * 2]
+        
+    }
+
+    // packet is unused
+    fn alu_stateful_fn( state_vars: &mut Vec<StateVar>,
+                        _packet : &Vec<PhvContainer<i32>>) -> Vec <i32>{
+        let old_state : Vec <i32> = state_vars.clone();
+        state_vars [0] = state_vars[0] + 2;
+        old_state
+    }
+
+    // Picks the first phv container to input
+    // into ALU 
+    let alu_one_one_input_mux_index_hole = 0 ;
+    let alu_one_two_input_mux_index_hole = 0 ;
+    // Picks the third input which is the result from
+    // the stateless ALU (state vars vector has 2 elements)
+    let alu_one_one_output_mux_index_hole = 2 ;
+    let alu_one_two_output_mux_index_hole = 2 ;
+
+    let alu_two_one_input_mux_index_hole = 0 ;
+    let alu_two_two_input_mux_index_hole = 0 ;
+    // Picks the first state variable
+    let alu_two_one_output_mux_index_hole = 0 ;
+    let alu_two_two_output_mux_index_hole = 0 ;
+
+    //arbitrary state variables
+    let mut state_vars : Vec<StateVar> = Vec::new();
+    state_vars.push(0);
+    state_vars.push(1);
+
+
+    /* Stage 1 */
+    let first_phv : Phv<i32> = Phv{bubble: true, packets: Vec::new()};
+
+    //generate input and output muxes for both ALUs in the first stage
+    
+    let alu_one_one_input_muxes : Vec<InputMux> = 
+        vec![InputMux {input_phv: first_phv.clone() , index : alu_one_one_input_mux_index_hole}];
+    let alu_one_two_input_muxes : Vec<InputMux> = 
+        vec![InputMux{input_phv: first_phv.clone() , index : alu_one_two_input_mux_index_hole}];
+    let alu_one_one_output_mux : OutputMux = OutputMux{input_phv_containers: Vec::new() , index: alu_one_one_output_mux_index_hole};
+
+    let alu_one_two_output_mux : OutputMux = OutputMux{input_phv_containers: Vec::new() , index: alu_one_two_output_mux_index_hole};
+
+    //generate ALUs for first pipeline stage
+   
+    let alu_one_one : ALU = 
+        ALU { sequential_function: Box::new(alu_stateful_fn), 
+              state_variables: state_vars.clone(), 
+              input_muxes: alu_one_one_input_muxes, 
+              output_mux: alu_one_one_output_mux, 
+              is_stateful : true };
+    let alu_one_two : ALU = 
+        ALU { sequential_function: Box::new(alu_stateless_fn), 
+              state_variables: Vec::new(), 
+              input_muxes: alu_one_two_input_muxes, 
+              output_mux: alu_one_two_output_mux, 
+              is_stateful : false };
+    
+    let pipeline_stage_one : PipelineStage = PipelineStage { 
+        stateful_atoms: vec![alu_one_one], 
+        stateless_atoms : vec![alu_one_two] };
+
+    /* Stage 2 */
+    let second_phv : Phv<i32> = Phv {bubble: true, packets: Vec::new()};
+
+    //generate input and output muxes for both ALUs in the second stage
+    
+    let alu_two_one_input_muxes : Vec<InputMux> = 
+        vec![InputMux{input_phv: second_phv.clone() , index : alu_two_one_input_mux_index_hole}];
+    let alu_two_two_input_muxes : Vec<InputMux> = 
+        vec![InputMux{input_phv: second_phv.clone() , index : alu_two_two_input_mux_index_hole}];
+    let alu_two_one_output_mux : OutputMux = 
+        OutputMux{input_phv_containers: Vec::new() , index: alu_two_one_output_mux_index_hole};
+    let alu_two_two_output_mux : OutputMux = 
+        OutputMux{input_phv_containers: Vec::new() , index: alu_two_two_output_mux_index_hole};
+
+    //generate ALUs for second pipeline stage
+    
+    let alu_two_one : ALU = 
+        ALU { sequential_function: Box::new(alu_stateful_fn), 
+              state_variables: state_vars.clone(), 
+              input_muxes : alu_two_one_input_muxes, 
+              output_mux: alu_two_one_output_mux, 
+              is_stateful : true };
+    let alu_two_two : ALU = 
+        ALU { sequential_function: Box::new(alu_stateless_fn), 
+              state_variables: Vec::new(), 
+              input_muxes : alu_two_two_input_muxes, 
+              output_mux: alu_two_two_output_mux, 
+              is_stateful: false 
+        };
+
+    let pipeline_stage_two : PipelineStage = PipelineStage{
+        stateful_atoms: vec![alu_two_one], 
+        stateless_atoms: vec![alu_two_two]
+    };
+    //generate pipeline
+    let mut pipeline : Pipeline = Pipeline::with_pipeline_stages(
+        vec![pipeline_stage_one, pipeline_stage_two]);
+
+
+    let field_values : Vec<i32> = vec![1, 2, 3, 4, 5, 6,
+                                       7, 8, 9, 10, 11, 12, 
+                                       13, 14, 15, 16, 17, 18,
+                                       19, 20];
+    
+    let ticks : i32 = 20;
+    let mut output_phvs : Vec<Phv<i32>> = Vec::new();
+    for t in 0..ticks {
+    
+        let mut packet : Phv<i32> = Phv::new();
+
+        packet.add_container_to_phv (PhvContainer {
+            field_value : field_values [(t as usize)],
+        });
+        let new_packet : Phv<i32> = pipeline.tick (packet);
+        if !new_packet.is_bubble() {
+          output_phvs.push (new_packet);
+        }
+    }
+    // Assert that the PhvContainer in every Phv is equal
+    // to 2 * the index of the current phv. This is because 
+    // the output muxes for the last pipeline stage have 
+    // their hole values set to 0, which selects the state
+    // first state variable. This state variable gets 
+    // multiplied by 2 every time the stateful ALU gets 
+    // run
+    let mut count : i32 = 0;
+    for p in &output_phvs {
+        assert! (p[0].get_value() == 
+                 count*2);
+        count+=1;
     }
 }
 
