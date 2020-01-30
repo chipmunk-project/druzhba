e92eb92c8853e79408fa4e4b4c9d646091456c0e
Mike Wong <mdw362@nyu.edu>
2020-01-30 14:36:38 -0500

    Added match/action parsing from dRMT scheduler output

--- src/scheduler.rs
+++ src/scheduler.rs
@@ -1,7 +1,6 @@
-use crate::phv::Phv;
-use std::env;
 use std::fs;
 use std::process::Command;
+use std::collections::HashMap;
 
 pub struct Scheduler {
     pub input_file : String,
@@ -10,6 +9,8 @@ pub struct Scheduler {
 impl Scheduler {
 
 
+  // Runs dRMT scheduler and parses output to get
+  // matches/actions at certain cycles
   pub fn exec_drmt_scheduler (&self) {
      let output = Command::new("p4-graphs")
                   .arg("--split-match-action-events")
@@ -44,19 +45,97 @@ impl Scheduler {
                                  .arg("/home/mikewong/Documents/drmt")
                                  .output()
                                  .expect("Could not run DRMT scheduler");
-/*
-     let drmt_scheduler_output = Command::new("python")
-                                 .arg("-u")
-                                 .arg("../drmt/drmt.py")
-                                 .arg(format!("{}", py_file))
-                                 .arg("../drmt/large_hw")
-                                 .arg("../drmt/drmt_latencies") 
-                                 .arg("10")
-                                 .output()
-                                 .expect("Could not run DRMT scheduler");
-*/
-    println!("dRMT result: {}", 
-              String::from_utf8_lossy(&drmt_scheduler_output.stdout));
+    let drmt_scheduler_result : String = 
+      String::from_utf8_lossy (&drmt_scheduler_output.stdout)
+      .to_string();
+    println!("dRMT result: {}", drmt_scheduler_result);
+    // Unfortuately dRMT scheduler sometimes prints
+    // the '\n' escape sequence instead of a newline.
+    // This if statement catches this behavior
+    let corrected_drmt_scheduler_result : String = 
+      str::replace(
+        &str::replace(&drmt_scheduler_result, "|\\n|", "|\n|"),
+        "|\\n\\n|", "|\n\n|").to_string();
+
+     let drmt_scheduler_result_vec : Vec <String> = 
+                              corrected_drmt_scheduler_result
+                             .split ("\n")
+                             .map (|s| s.to_string())
+                             .collect();
+
+    let mut cycles_to_matches_and_actions : HashMap <i32, Vec<String> >  = 
+      HashMap::new();
+    // Parse through dRMT result file and get matches/actions
+    // and the cycles that they occur in
+    for i in 0..drmt_scheduler_result_vec.len() {
+      let mut line : &str = &drmt_scheduler_result_vec[i];
+      // Enter the portion of the file that contains schedule
+      if line.contains("scheduling period on one processor"){
+
+        let mut offset : usize = 1;
+        line = &drmt_scheduler_result_vec[i + offset];
+
+        let mut temp_cycles : Vec <i32> = Vec::new();
+        while !line.contains("Steady state"){
+
+          // Contains a line in the schedule
+          let line_vec : Vec <&str> = line
+                                       .split("|")
+                                       .map(|s| s.trim())
+                                       .collect();
+          // The scheduler prints a line of numerical cycles.
+          // Below these values are the corresponding matches/actions
+          // to be performed at that cycle.
+          // First, get the numerical cycle values
+          if line.contains("t="){
+            temp_cycles.clear();
+            for j in 0..line_vec.len() {
+              if !&line_vec[j].contains("t="){
+                continue;
+              }
+              let mut chars = line_vec[j].chars();
+              chars.next();
+              chars.next();
+              let cycle = chars.as_str()
+                               .to_string()
+                               .parse::<i32>()  
+                               .unwrap();
+              if j >= temp_cycles.len() {
+                temp_cycles.push (cycle);
+                cycles_to_matches_and_actions
+                  .insert(cycle, Vec::new());
+              }
+              else {
+                temp_cycles[j] = cycle;
+              }
+              
+            }
+          }
+          // Get the matches and actions that are used in
+          // every cycle after the cycle values have been
+          // found
+          else {
+            // First element is empty string
+            for j in 0..line_vec.len(){
+              let schedule_item : &str = &line_vec[j].trim();
+              if schedule_item.contains("\\n\\n") || 
+                 schedule_item == "" {
+                continue;
+              }
+              let mut current_vec : Vec <String> = 
+                cycles_to_matches_and_actions[&temp_cycles[*&j - 1]].clone();
+              current_vec.push(schedule_item.to_string());
+              cycles_to_matches_and_actions
+                .insert(temp_cycles[*&j - 1], current_vec);
+            }
+          }
+          offset += 1;
+          line = &drmt_scheduler_result_vec[i + offset];
+        }
+
+      }
+    }
+    println!("{:?}", cycles_to_matches_and_actions);
 
   }
 }
