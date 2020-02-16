1a55e58feda4655be47033b3190559a4fcc69df4
Mike Wong <mdw362@nyu.edu>
2020-02-15 23:51:45 -0500

    Attempt to mitigate appveyor timeout

--- init_tmp_files.sh
+++ init_tmp_files.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-cd dgen && cargo build 
-cargo run pisa simple ../example_alus/stateful_alus/raw.alu ../example_alus/stateless_alus/stateless_alu.alu 2 2 1 "0,1,2,3" ../src/prog_to_run.rs
+cd dgen && cargo build
+cp target/debug/dgen dgen_bin
+chmod +x dgen_bin
+./dgen_bin pisa simple ../example_alus/stateful_alus/raw.alu ../example_alus/stateless_alus/stateless_alu.alu 2 2 1 "0,1,2,3" ../src/prog_to_run.rs
 
-cargo run dRMT ../p4_files/stateful.p4 /random/incorrect/path/ ../src/match_action_ops.rs
+./dgen_bin dRMT ../p4_files/stateful.p4 /random/incorrect/path/ ../src/match_action_ops.rs
 cd ..
 
